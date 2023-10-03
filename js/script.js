9e922af18627146ab2495a4885d5a81632cbd044
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-10-03 18:01:04 +0200

    separated functions and script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import Note from "./notes.js";
+import function from "./functions.js";
 
 const data = [
   { name: "do", audio: new Audio("./../audio/do.mp3") },
@@ -19,12 +20,12 @@ const startGameBtn = document.getElementById("start");
 const startScreen = document.getElementById("start-screen");
 const menuScreen = document.getElementById("menu-screen");
 
-const warmUpBtn = document.getElementById("warm-up");
 const singleNote = document.getElementById("single-note");
 const triplet = document.getElementById("triplet");
 
 const playScreen = document.getElementById("play-screen");
 const counter = document.querySelector("#time span");
+counter.textContent = count;
 const score = document.querySelector("#score span");
 
 const playShortBtn = document.getElementById("play-short");
@@ -39,26 +40,32 @@ scale.addEventListener("click", () => {
   playTheScale(notes);
 });
 
-const warmUpOverBtn = document.getElementById("warm-up-over");
-
 startGameBtn.addEventListener("click", startGame);
-
-function startGame() {
-  startScreen.classList.add("hidden");
-  menuScreen.classList.remove("hidden");
-}
 
 menuScreen.addEventListener("click", () => {
   menuScreen.classList.add("hidden");
   playScreen.classList.remove("hidden");
 });
 
-let canPlay = true;
-let scoring = 0;
-
 singleNote.addEventListener("click", () => {
   startSingleNote();
 
+  const intervalId = setInterval(() => {
+    count--;
+
+    score.textContent = scoring;
+
+    if (count === 0) {
+      //GAME OVER MESSAGE TO ADD
+      counter.textContent = count;
+      canPlay = false;
+      clearInterval(intervalId);
+    }
+  }, 1000);
+});
+
+triplet.addEventListener("click", () => {
+  startTriplet();
   const intervalId = setInterval(() => {
     count--;
     counter.textContent = count;
@@ -71,113 +78,3 @@ singleNote.addEventListener("click", () => {
     }
   }, 1000);
 });
-
-function startSingleNote() {
-  canPlay = true;
-  const randomNote = notes[Math.floor(Math.random() * notes.length)];
-
-  notesToPlay.push(randomNote);
-  console.log(notesToPlay);
-  notesToPlay[0].play();
-
-  const notesBtn = document.querySelectorAll(".notesBtn");
-  for (let i = 0; i < notesBtn.length; i++) {
-    notesBtn[i].addEventListener("click", () => {
-      if (notesBtn[i].classList.contains("clicked") || !canPlay) return;
-      notesBtn[i].classList.add("clicked");
-
-      if (clickedBtns.length < 1) {
-        clickedBtns.push(notesBtn[i]);
-
-        if (clickedBtns.length === 1) {
-          canPlay = false;
-
-          checkIfCorrect(clickedBtns);
-        }
-      }
-    });
-  }
-}
-
-function checkIfCorrect(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (notesToPlay[i].name === arr[i].id) {
-      scoring++;
-      console.log("Yesss");
-      arr[i].classList.add("right-answer");
-
-      const okSound = new Audio("./../audio/right-answer.mp3");
-      okSound.playbackRate = 3;
-      okSound.play();
-    } else {
-      console.log("Nooo");
-
-      arr[i].classList.add("wrong-answer");
-      const expectedAnswer = document.getElementById(notesToPlay[i].name);
-      expectedAnswer.classList.add("expected-answer");
-      setTimeout(() => {
-        expectedAnswer.classList.remove("expected-answer");
-      }, 1300);
-
-      const noSound = new Audio("./../audio/wrong-answer.mp3");
-      noSound.playbackRate = 3;
-      noSound.play();
-    }
-  }
-  setTimeout(() => {
-    notesToPlay = [];
-    canPlay = true;
-    resetClass();
-    clickedBtns = [];
-    startSingleNote();
-  }, 1300);
-}
-
-function resetClass() {
-  clickedBtns.forEach((btn) => {
-    btn.classList.remove(
-      "clicked",
-      "right-answer",
-      "wrong-answer",
-      "expected-answer"
-    );
-  });
-}
-
-triplet.addEventListener("click", () => {
-  startTriplet();
-});
-
-function startTriplet() {
-  canPlay = true;
-  for (let i = 0; i < 3; i++) {
-    const randomNote = notes[Math.floor(Math.random() * notes.length)];
-
-    notesToPlay.push(randomNote);
-    console.log(notesToPlay);
-  }
-  playTheScale(notesToPlay);
-}
-
-function playShort() {
-  notesToPlay.forEach((note) => note.short());
-  playTheScale(notesToPlay);
-}
-
-function playLong() {
-  notesToPlay.forEach((note) => note.long());
-  playTheScale(notesToPlay);
-}
-
-function playTheScale(arr, index = 0) {
-  if (index === arr.length) return;
-  console.log(arr);
-  arr[index].play();
-  arr[index].audio.addEventListener(
-    "ended",
-    () => {
-      playTheScale(arr, index + 1);
-    },
-    { once: true }
-  );
-}
