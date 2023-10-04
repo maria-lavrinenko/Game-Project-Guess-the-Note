474335cd2b1af1b8cfce54e3a31767b668aef8e4
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-10-04 10:31:50 +0200

    added a modal and gameOver function

--- js/functions.js
+++ js/functions.js
@@ -1,136 +1,17 @@
-let canPlay = true;
-let scoring = 0;
+import Note from "./notes.js";
+import { playScreen, menuScreen } from "./script.js";
 
-function startGame() {
-  startScreen.classList.add("hidden");
-  menuScreen.classList.remove("hidden");
+export function playShort(arr) {
+  arr.forEach((note) => note.short());
+  playTheScale(arr);
 }
 
-function startSingleNote() {
-  canPlay = true;
-  randomChoice();
-
-  const notesBtn = document.querySelectorAll(".notesBtn");
-  for (let i = 0; i < notesBtn.length; i++) {
-    notesBtn[i].addEventListener("click", () => {
-      if (notesBtn[i].classList.contains("clicked") || !canPlay) return;
-      notesBtn[i].classList.add("clicked");
-
-      if (clickedBtns.length < 1) {
-        clickedBtns.push(notesBtn[i]);
-
-        if (clickedBtns.length === 1) {
-          canPlay = false;
-
-          checkIfCorrect(clickedBtns);
-          setTimeout(() => {
-            notesToPlay = [];
-            canPlay = true;
-            resetClass();
-            clickedBtns = [];
-            startSingleNote();
-          }, 1300);
-        }
-      }
-    });
-  }
+export function playLong(arr) {
+  arr.forEach((note) => note.long());
+  playTheScale(arr);
 }
 
-function randomChoice() {
-  const randomNote = notes[Math.floor(Math.random() * notes.length)];
-
-  notesToPlay.push(randomNote);
-  notesToPlay.play();
-}
-
-function checkIfCorrect(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (notesToPlay[i].name === arr[i].id) {
-      scoring++;
-      console.log("Yesss");
-      arr[i].classList.add("right-answer");
-
-      const okSound = new Audio("./../audio/right-answer.mp3");
-      okSound.playbackRate = 3;
-      okSound.play();
-    } else {
-      console.log("Nooo");
-
-      arr[i].classList.add("wrong-answer");
-      const expectedAnswer = document.getElementById(notesToPlay[i].name);
-      expectedAnswer.classList.add("expected-answer");
-      setTimeout(() => {
-        expectedAnswer.classList.remove("expected-answer");
-      }, 1300);
-
-      const noSound = new Audio("./../audio/wrong-answer.mp3");
-      noSound.playbackRate = 3;
-      noSound.play();
-    }
-  }
-}
-
-function resetClass() {
-  clickedBtns.forEach((btn) => {
-    btn.classList.remove(
-      "clicked",
-      "right-answer",
-      "wrong-answer",
-      "expected-answer"
-    );
-    btn.disabled = false;
-  });
-}
-
-function startTriplet() {
-  canPlay = true;
-  for (let i = 0; i < 3; i++) {
-    const randomNote = notes[Math.floor(Math.random() * notes.length)];
-
-    notesToPlay.push(randomNote);
-    randomNote.normal();
-  }
-
-  playTheScale(notesToPlay);
-
-  const notesBtn = document.querySelectorAll(".notesBtn");
-  for (let i = 0; i < notesBtn.length; i++) {
-    notesBtn[i].addEventListener("click", () => {
-      if (notesBtn[i].classList.contains("clicked") || !canPlay) return;
-      notesBtn[i].classList.add("clicked");
-      notesBtn[i].disabled = true;
-
-      if (clickedBtns.length < 3) {
-        clickedBtns.push(notesBtn[i]);
-
-        if (clickedBtns.length === 3) {
-          canPlay = false;
-
-          checkIfCorrect(clickedBtns);
-          setTimeout(() => {
-            notesToPlay = [];
-            canPlay = true;
-            resetClass();
-            clickedBtns = [];
-            startTriplet();
-          }, 1300);
-        }
-      }
-    });
-  }
-}
-
-function playShort() {
-  notesToPlay.forEach((note) => note.short());
-  playTheScale(notesToPlay);
-}
-
-function playLong() {
-  notesToPlay.forEach((note) => note.long());
-  playTheScale(notesToPlay);
-}
-
-function playTheScale(arr, index = 0) {
+export function playTheScale(arr, index = 0) {
   if (index === arr.length) return;
   // console.log(arr);
   arr[index].play();
@@ -143,4 +24,15 @@ function playTheScale(arr, index = 0) {
   );
 }
 
-export default functions;
+export function gameOver() {
+  const dialog = document.createElement("dialog");
+  playScreen.append(dialog);
+
+  dialog.src = "./../styles/press-any-key.jpeg";
+  dialog.showModal();
+
+  dialog.addEventListener("keypress", () => {
+    playScreen.classList.add("hidden");
+    menuScreen.classList.remove("hidden");
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import Note from "./notes.js";
-import function from "./functions.js";
+import * as functions from "./functions.js";
 
 const data = [
   { name: "do", audio: new Audio("./../audio/do.mp3") },
@@ -12,35 +12,44 @@ const data = [
 ];
 const notes = data.map((noteData) => new Note(noteData.name, noteData.audio));
 
-let notesToPlay = [];
+let time = 30;
+let scoring = 0;
+let canPlay = true;
 let clickedBtns = [];
-let count = 30;
+let notesToPlay = [];
 
 const startGameBtn = document.getElementById("start");
 const startScreen = document.getElementById("start-screen");
-const menuScreen = document.getElementById("menu-screen");
+export const menuScreen = document.getElementById("menu-screen");
 
 const singleNote = document.getElementById("single-note");
 const triplet = document.getElementById("triplet");
 
-const playScreen = document.getElementById("play-screen");
-const counter = document.querySelector("#time span");
-counter.textContent = count;
+export const playScreen = document.getElementById("play-screen");
+
+const timer = document.querySelector("#time span");
+timer.textContent = time;
+
 const score = document.querySelector("#score span");
 
 const playShortBtn = document.getElementById("play-short");
-playShortBtn.addEventListener("click", playShort);
+playShortBtn.addEventListener("click", functions.playShort(notesToPlay));
 
 const playLongBtn = document.getElementById("play-long");
-playLongBtn.addEventListener("click", playLong);
+playLongBtn.addEventListener("click", functions.playLong(notesToPlay));
 
 const scale = document.getElementById("scale");
 scale.addEventListener("click", () => {
   notes.forEach((note) => note.normal());
-  playTheScale(notes);
+  functions.playTheScale(notes);
 });
 
 startGameBtn.addEventListener("click", startGame);
+
+function startGame() {
+  startScreen.classList.add("hidden");
+  menuScreen.classList.remove("hidden");
+}
 
 menuScreen.addEventListener("click", () => {
   menuScreen.classList.add("hidden");
@@ -51,30 +60,143 @@ singleNote.addEventListener("click", () => {
   startSingleNote();
 
   const intervalId = setInterval(() => {
-    count--;
-
+    time--;
+    timer.textContent = time;
     score.textContent = scoring;
 
-    if (count === 0) {
-      //GAME OVER MESSAGE TO ADD
-      counter.textContent = count;
+    if (time === 0) {
       canPlay = false;
+
       clearInterval(intervalId);
+      functions.gameOver();
     }
   }, 1000);
 });
+
+function startSingleNote() {
+  canPlay = true;
+  randomChoice();
+
+  notesToPlay[0].play();
+
+  const notesBtn = document.querySelectorAll(".notesBtn");
+  for (let i = 0; i < notesBtn.length; i++) {
+    notesBtn[i].addEventListener("click", () => {
+      if (notesBtn[i].classList.contains("clicked") || !canPlay) return;
+      notesBtn[i].classList.add("clicked");
+
+      if (clickedBtns.length < 1) {
+        clickedBtns.push(notesBtn[i]);
+
+        if (clickedBtns.length === 1) {
+          canPlay = false;
+
+          checkIfCorrect(clickedBtns);
+          setTimeout(() => {
+            notesToPlay = [];
+            canPlay = true;
+            resetClass();
+            clickedBtns = [];
+            startSingleNote();
+          }, 1300);
+        }
+      }
+    });
+  }
+}
+
+function randomChoice() {
+  const randomNote = notes[Math.floor(Math.random() * notes.length)];
+  notesToPlay.push(randomNote);
+}
 
 triplet.addEventListener("click", () => {
   startTriplet();
   const intervalId = setInterval(() => {
-    count--;
-    counter.textContent = count;
+    time--;
+    timer.textContent = time;
     score.textContent = scoring;
 
-    if (count === 0) {
-      //GAME OVER MESSAGE TO ADD
+    if (time === 0) {
+      functions.gameOver();
+
       canPlay = false;
       clearInterval(intervalId);
     }
   }, 1000);
 });
+
+function startTriplet() {
+  canPlay = true;
+  for (let i = 0; i < 3; i++) {
+    randomChoice();
+  }
+
+  functions.playTheScale(notesToPlay);
+
+  const notesBtn = document.querySelectorAll(".notesBtn");
+  for (let i = 0; i < notesBtn.length; i++) {
+    notesBtn[i].addEventListener("click", () => {
+      if (notesBtn[i].classList.contains("clicked") || !canPlay) return;
+      notesBtn[i].classList.add("clicked");
+      notesBtn[i].disabled = true;
+
+      if (clickedBtns.length < 3) {
+        clickedBtns.push(notesBtn[i]);
+
+        if (clickedBtns.length === 3) {
+          canPlay = false;
+
+          checkIfCorrect(clickedBtns);
+          setTimeout(() => {
+            notesToPlay = [];
+            canPlay = true;
+            resetClass();
+            clickedBtns = [];
+            startTriplet();
+          }, 1300);
+        }
+      }
+    });
+  }
+}
+
+function checkIfCorrect(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (notesToPlay[i].name === arr[i].id) {
+      scoring++;
+      score.textContent = scoring;
+      console.log("Yesss");
+      arr[i].classList.add("right-answer");
+
+      const okSound = new Audio("./../audio/right-answer.mp3");
+      okSound.playbackRate = 3;
+      okSound.play();
+    } else {
+      console.log("Nooo");
+
+      arr[i].classList.add("wrong-answer");
+      const expectedAnswer = document.getElementById(notesToPlay[i].name);
+      expectedAnswer.classList.add("expected-answer");
+      setTimeout(() => {
+        expectedAnswer.classList.remove("expected-answer");
+      }, 1300);
+
+      const noSound = new Audio("./../audio/wrong-answer.mp3");
+      noSound.playbackRate = 3;
+      noSound.play();
+    }
+  }
+}
+
+function resetClass() {
+  clickedBtns.forEach((btn) => {
+    btn.classList.remove(
+      "clicked",
+      "right-answer",
+      "wrong-answer",
+      "expected-answer"
+    );
+    btn.disabled = false;
+  });
+}
